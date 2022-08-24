d89a31be0892af408ca3d145163dde49407d39ea
Omarvillada <olvillada@gmail.com>
2022-08-23 21:29:51 -0500

    add cross origin

--- src/main/java/com/buda/Bingo/web/controller/BalotaController.java
+++ src/main/java/com/buda/Bingo/web/controller/BalotaController.java
@@ -4,6 +4,7 @@ import com.buda.Bingo.model.ColumnasBingo;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import java.util.Map;
 @RequestMapping("/jugar")
 public class BalotaController {
 
+    @CrossOrigin(origins = "http://localhost:4200/")
     @GetMapping("")
     public ResponseEntity<Object> generateBalota(){
         String number = GeneratePlay.getNumberBalota();
--- src/main/java/com/buda/Bingo/web/controller/TableroController.java
+++ src/main/java/com/buda/Bingo/web/controller/TableroController.java
@@ -3,6 +3,7 @@ package com.buda.Bingo.web.controller;
 import com.buda.Bingo.model.Tablero;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/tablero")
 public class TableroController {
-
+    @CrossOrigin(origins = "http://localhost:4200/")
     @GetMapping("")
     public ResponseEntity<Tablero> getTablero(){
         return new ResponseEntity<>(
